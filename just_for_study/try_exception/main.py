cf9d52d76a0f5030d5255fa9a27fccff62ab36e3
ChenMiaoi <3026066798@qq.com>
2022-04-21 23:31:09 +0800

    add some code

--- just_for_study/try_exception/main.py
+++ just_for_study/try_exception/main.py
@@ -10,6 +10,7 @@
     如果try中异常了，从异常开始的以后的代码就会被跳过直接执行异常处理
 '''
 
+import imp
 from tokenize import Name
 
 
@@ -55,4 +56,45 @@ finally :
 # ! else结构，如果没有异常就执行else，有异常就执行except
 
 # ! finally结构，finally结构内的代码无论如何都会被执行，通常用来释放try中申请的资源
+# ! 尽量不要再抛异常内使用return语句
 
+# ! 常见错误汇总
+'''
+    # ? SyntaxError : 语法错误
+    # ? NameError : 尝试访问一个没有声明的变量
+    # ? ZeroDivisionError : 除数为0
+    # ? ValueError : 数值错误
+    # ? TypeError : 类型错误
+    # ? AttributeError : 访问对象的不存在属性
+    # ? IndexError : 索引越界异常
+    # ? KeyError : 字典的关键字不存在
+    # ? ArithmeticError : 所有数值计算错误的基类
+    # ? AssertionError : 断言语句失败
+    # ? BaseException : 所有异常的基类
+    # ? DeprecationWarning : 关于被弃用的特征的警告
+    # ? EnvironmentError : 操作系统错误的基类
+    # ? EOFError : 没有内建输入,到达EOF标记
+    # ? Exception : 常规错误的基类
+    # ? FloatingPoiontError : 浮点计算错误
+    # ? FutureWaring : 关于构造将来语义会有改变的警告
+    # ? GeneratorExit : 生成器发生异常来通知退出
+    # ? ImportError : 导入模块/对象失败
+    ...
+'''
+# ! 上下文管理器 -- with
+# TODO with context_expr[as var] :
+
+# ! trackback模块
+import traceback
+try :
+    print("step1")
+    num = 1 / 0
+except :
+    traceback.print_exc()
+
+# TODO 将异常信息输出到指定文件
+try :
+    print("step1")
+except :
+    with open(...) as f :
+        traceback.print_exc(file = f)
