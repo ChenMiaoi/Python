1689e1e66ccefd734c6e7c7e425700b6bb445584
ChenMiaoi <3026066798@qq.com>
2022-07-02 20:55:32 +0800

    The activation functions

---
+++ deepinglearning/NeuralNetWorks/Activation/activation.py
@@ -0,0 +1,55 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def sigmoid(x) :
+    w = 1
+    b = 1
+    z = w * x + b
+    sig = 1 / (1 + np.exp(-z))
+    plt.plot(x, sig)
+    plt.title("The sigmoid function data", fontsize = 16)
+    plt.xlabel("x datas")
+    plt.ylabel("sigmoid()")
+    plt.grid()
+    plt.axhline(y=0.5, ls = '--', c = 'red')
+    plt.show()
+
+def tanh(x) :
+    w = 1
+    b = 1
+    z = w * x + b
+    act = (np.exp(z) - np.exp(-z)) / (np.exp(z) + np.exp(-z))
+    plt.plot(x, act)
+    plt.title("The tanh function data", fontsize=16)
+    plt.xlabel("x datas")
+    plt.ylabel("tanh()")
+    plt.grid()
+    plt.axhline(y=0.0, ls='--', c='red')
+    plt.savefig("tanh.png")
+    plt.show()
+
+def _max(x) :
+    z = max(0, x.any())
+    return z
+
+def ReLU(x) :
+    w = 1
+    b = 1
+    z = w * x + b
+    act = _max(x)
+    plt.plot(x, act)
+    plt.title("The tanh function data", fontsize=16)
+    plt.xlabel("x datas")
+    plt.ylabel("tanh()")
+    plt.grid()
+    plt.axhline(y=0.0, ls='--', c='red')
+    plt.savefig("ReLU.png")
+    plt.show()
+
+if __name__ == "__main__" :
+    print("sigmoid function")
+    x_data = np.arange(-10.0, 10.0, 0.1)
+    # sigmoid(x_data)
+    # tanh(x_data)
+    ReLU(x_data)
